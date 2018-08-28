520645c313e62bbc31a5013a29432e3ea56d8a3b
Austin Lippert <6955342+aternex@users.noreply.github.com>
2018-08-28 11:37:27 -0400

    Hack the authorized funds to handle a different use case for finance and finance view only roles

--- CmsData/Extensions/ContributionFundExtensions.cs
+++ CmsData/Extensions/ContributionFundExtensions.cs
@@ -15,20 +15,39 @@ namespace CmsData
         {
             if (allowedRoles != null)
             {
+                // get list of role names used as fund manager roles
+                var limitingRoles = contributionFunds
+                    .Join(DbUtil.Db.Roles, fund => fund.FundManagerRoleId, role => role.RoleId, (fund, role) => new { fund, role })
+                    .Where(x => x.fund.FundManagerRoleId != 0)
+                    .Select(x => x.role.RoleName)
+                    .Distinct();
+
+                // having the finance role gives you access to any and every fund
                 if (allowedRoles.Contains("Finance"))
                 {
                     return contributionFunds;
                 }
 
+                // having the financeviewonly role is multipurpose, by itself you have everything... paired w/ any limiting role and its just those funds
                 if (allowedRoles.Contains("FinanceViewOnly"))
                 {
-                    return contributionFunds.Where(f => f.FundManagerRoleId != 0)
-                        .Join(DbUtil.Db.Roles, f => f.FundManagerRoleId, r => r.RoleId, (f, r) => new { role = r, fund = f })
-                        .Where(r => allowedRoles.Contains(r.role.RoleName))
-                        .Select(r => r.fund);
+                    var hasLimitingRoles = limitingRoles.Intersect(allowedRoles).Any();
+
+                    if (!hasLimitingRoles)
+                    {
+                        return contributionFunds;
+                    }
+                    else
+                    {
+                        return contributionFunds.Where(f => f.FundManagerRoleId != 0)
+                            .Join(DbUtil.Db.Roles, f => f.FundManagerRoleId, r => r.RoleId, (f, r) => new { role = r, fund = f })
+                            .Where(r => allowedRoles.Contains(r.role.RoleName))
+                            .Select(r => r.fund);
+                    }
                 }
             }
 
+            // didn't have any relevant role to manage funds so you wouldn't see anything in this list
             return GetEmptyList();
         }
 
